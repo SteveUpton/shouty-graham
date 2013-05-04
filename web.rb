3671632c6677f095586606feca3069c44aac2a79
Steve Upton <steve@steveupton.net>
2013-05-04 04:37:51 +0100

    Initial switch to full Ruby

---
+++ web.rb
@@ -0,0 +1,27 @@
+require 'twitter'
+require 'TweetStream'
+
+Twitter.configure do |config|
+  config.consumer_key = ENV['sg_consumer_key']
+  config.consumer_secret = ENV['sg_consumer_secret']
+  config.oauth_token = ENV['sg_oauth_token']
+  config.oauth_token_secret = ENV['sg_oauth_token_secret']
+end
+
+TweetStream.configure do |config|
+  config.consumer_key = ENV['sg_consumer_key']
+  config.consumer_secret = ENV['sg_consumer_secret']
+  config.oauth_token = ENV['sg_oauth_token']
+  config.oauth_token_secret = ENV['sg_oauth_token_secret']
+  config.auth_method        = :oauth
+end
+
+# 281615153 Actual Graham
+
+# 1401150828 test272637 
+
+TweetStream::Client.new.follow(1401150828) do |status|
+  shoutytweet = status.text.upcase	# Shoutify
+  shoutytweet.slice! "@"			# Strip out @s
+  Twitter.update(shoutytweet)		# Tweet
+end
